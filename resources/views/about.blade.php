67ea39d02ffe2159a08e40bda49081ffae04ded0
fatikhul36 <fatihmusyafa72@gmail.com>
2022-01-18 07:45:44 +0700

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>fatih | about</title>
+</head>
+<body>
+    <h1>halaman about</h1>
+    <h3> { { $nama } }</h3>
+    <p> {{ $email }}</p>
+    <img src="{{$gamabar}}" alt="{{$nama}}" width="200px">
+</body>
+</html>
